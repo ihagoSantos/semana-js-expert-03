e1ba1dca1c9e2f544dec1ab058473429bfef7c04
Ihago <ihagog12@gmail.com>
2021-03-23 22:45:06 -0300

    websocket

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,9 +1,24 @@
+/**
+node index.js \
+    --username ihagosantos \
+    --room sala01 \
+    --hostUri localhost
+*/
+
 import Events from 'events';
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from "./src/terminalController.js";
 
-const componentEmitter = new Events();
-const controller = new TerminalController();
+const [nomePath, filePath, ...commands] = process.argv;
 
-await controller.initializeTable(componentEmitter);
+const config = CliConfig.parseArguments(commands);
+
+const componentEmitter = new Events();
+
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
 
 
