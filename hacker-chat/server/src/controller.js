d029a16ec850210149996c1c88aac754527d421c
Ihago <ihagog12@gmail.com>
2021-03-26 20:21:52 -0300

    removendo node modules

--- hacker-chat/server/src/controller.js
+++ hacker-chat/server/src/controller.js
@@ -54,6 +54,18 @@ export default class Controller {
         }
     }
 
+    message(id, data){
+        const { userName, roomId } = this.#users.get(id);
+        
+        this.broadCast({
+            id,
+            roomId,
+            event: constants.event.MESSAGE,
+            message: { userName, message: data},
+            includCurrentSocket: true
+        })
+    }
+
     #joinUserOnRoom(roomId, user){
         const usersOnRoom = this.#rooms.get(roomId) ?? new Map();
         usersOnRoom.set(user.id, user)
@@ -62,9 +74,27 @@ export default class Controller {
         return usersOnRoom
     }
 
+    #logoutUser(id, roomId){
+        this.#users.delete(id);
+        const usersOnRoom = this.#rooms.get(roomId)
+        usersOnRoom.delete(id)
+
+        this.#rooms.set(roomId, usersOnRoom)
+    }
+
     #onSocketClosed(id) {
-        return data => {
-            console.log('onSocketClosed', id)
+        return _ => {
+            const { userName, roomId } = this.#users.get(id);
+            console.log(userName, 'disconected', id);
+
+            this.#logoutUser(id, roomId)
+
+            this.broadCast({
+                roomId,
+                message: {id, userName},
+                socketId: id,
+                event: constants.event.DISCONNECT_USER,
+            })
         }
     }
     #onSocketData(id) {
